76a8b82e0da0d9c626eb8f140c11029f26febc0c
alberto <alberto.wagner@m2atecnologia.com.br>
2023-07-10 21:04:45 -0300

    atulizando celery para logging

--- backend_challenge/celery.py
+++ backend_challenge/celery.py
@@ -2,12 +2,25 @@ import os
 from celery import Celery
 
 # Configura a definição do Celery
+from celery.signals import setup_logging
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_challenge.settings')
 app = Celery('backend_challenge')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
 
+@setup_logging.connect
+def config_loggers(*args, **kwargs):
+    from logging.config import dictConfig  # noqa
+    from django.conf import settings  # noqa
+
+    dictConfig(settings.LOGGING)
+
+
+# Load task modules from all registered Django app configs.
 
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
+
+
+app.autodiscover_tasks()
