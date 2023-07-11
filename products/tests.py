2b67ddc53705c6cba748a8a0da2473b73cc43b76
alberto <alberto.wagner@m2atecnologia.com.br>
2023-07-10 21:00:50 -0300

    Atualizando testes

--- products/tests.py
+++ products/tests.py
@@ -1,4 +1,7 @@
 import unittest
+import json
+from datetime import datetime
+
 from django.test import TestCase
 from rest_framework.test import APIClient
 
@@ -18,7 +21,7 @@ class ProductAPITestCase(TestCase):
             'code': '3251490332080',
             'barcode': fake.ean13(),
             'status': 'imported',
-            'imported_t': "2023-07-09T00:53:57Z",
+            'imported_t': datetime.strptime("2023-07-09T00:53:57Z", "%Y-%m-%dT%H:%M:%SZ"),
             'url': fake.url(),
             'product_name': fake.name(),
             'quantity': fake.random_number(),
@@ -38,13 +41,14 @@ class ProductAPITestCase(TestCase):
     def test_list_products(self):
         response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, [self.serializer.data])
+        self.assertEqual(json.loads(response.content), [self.serializer.data])
 
     def test_retrieve_product(self):
         detail_url = reverse('product-detail', args=[3251490332080])
         response = self.client.get(detail_url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, self.serializer.data)
+        self.assertEqual(json.loads(response.content), self.serializer.data)
+
 
 if __name__ == '__main__':
     unittest.main()
