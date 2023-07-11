1bebf80eb64c535d1daf11bad8a28ae434f6d705
alberto <alberto.wagner@m2atecnologia.com.br>
2023-07-10 21:04:16 -0300

    removendo arquivos sem uso

--- backend_challenge/celerybeat.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-from django.conf import settings
-
-app = Celery('backend_challenge')
-app.config_from_object(settings, namespace='CELERY')
-app.conf.beat_schedule = {
-    # Defina aqui as tarefas agendadas
-}
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
