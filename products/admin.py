344dbae69331db25232827256648d5ed2c138cd2
alberto <alberto.wagner@m2atecnologia.com.br>
2023-07-10 21:05:02 -0300

    atulizando admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Product
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['code', 'product_name', 'quantity', 'categories', 'packaging', 'brands']
+    list_filter = ['status']
+    search_fields = ['code', 'product_name']
